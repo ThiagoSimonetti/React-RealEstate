c1e773ceec98f352fffc4b24857813297e89e675
Thiago Simonetti <tbtsimon7@gmail.com>
2022-01-30 22:08:42 -0800

    Added Directory Component Title-Subtitle

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -48,15 +48,41 @@ function Directory(props) {
   //   );
   // }
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h2>Directory</h2>
-          <hr />
+    <React.Fragment>
+      <div className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">Imóveis em Destaque</h1>
+            <p className="lead text-muted">
+              Lorem ipsum dolor sit amet, quando eruditi tacimates eu pri. Dicta
+              vocent democritum pri ea, per ut dicat atqui placerat. Quas soleat
+              hendrerit at his, ut eos veritus expetendis, modus.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="row">{directory}</div>
-    </div>
+      <div className="pb-5 px-5 featuredHomes">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+          {directory}
+        </div>
+      </div>
+    </React.Fragment>
+
+    // <div className="container">
+    //   <div className="row">
+    //     <div className="col text-center py-5">
+    //       <h1 className="fw-light">Imovéis em Destaque</h1>
+    //       <p className="lead text-muted">
+    //         Lorem ipsum dolor sit amet, quando eruditi tacimates eu pri. Dicta
+    //         vocent democritum pri ea, per ut dicat atqui placerat. Quas soleat
+    //         hendrerit at his, ut eos veritus expetendis, modus.
+    //       </p>
+    //       <hr />
+    //     </div>
+    //     <hr />
+    //   </div>
+    //   <div className="row">{directory}</div>
+    // </div>
   );
 }
 
